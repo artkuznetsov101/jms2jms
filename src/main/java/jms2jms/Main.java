1a4abbc6c631e6cccde0c9c0d3665ce37696d47c
Kuznetsov Artem <artkuznetsov101@gmail.com>
2018-10-10 18:41:35 +0300

    fix

--- src/main/java/jms2jms/Main.java
+++ src/main/java/jms2jms/Main.java
@@ -1,14 +1,10 @@
 package jms2jms;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
 
 import org.ini4j.Wini;
 
 public class Main {
-
-	private static SimpleDateFormat formatterWithTimezone = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:Z");
-
 	public static void main(String[] args) throws Exception {
 
 		Wini ini = new Wini(new File(Config.NAME));
@@ -20,9 +16,4 @@ public class Main {
 		jmsThread.start();
 		jmsThread.join();
 	}
-
-	public static String getTimestampWithTimezone() {
-		return formatterWithTimezone.format(System.currentTimeMillis());
-	}
-
 }
